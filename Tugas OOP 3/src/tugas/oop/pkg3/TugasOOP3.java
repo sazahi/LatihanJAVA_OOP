ccf1580ae4309cda10df3cb6541cf954299955b1
sazahi <103054130+sazahi@users.noreply.github.com>
2022-05-11 21:05:06 +0700

    Add files via upload

---
+++ Tugas OOP 3/src/tugas/oop/pkg3/TugasOOP3.java
@@ -0,0 +1,86 @@
+
+package tugas.oop.pkg3;
+class BangunDatar {
+    float luas(){
+        System.out.println("Menghitung luas bangun datar");
+        return 0;
+    }
+    
+    float keliling(){
+        System.out.println("Menghitung keliling bangun datar");
+        return 0;
+    }
+}
+class Persegi extends BangunDatar{
+    int sisi;
+    
+    public Persegi(int sisi) {
+        this.sisi = sisi;
+    }
+    
+    @Override
+    public float luas() {
+        return this.sisi * this.sisi;
+    }
+    
+    @Override
+    public float keliling(){
+        return this.sisi * 4;
+    }
+}
+class Segitiga extends BangunDatar{
+   int alas;
+   int tinggi;
+
+    public Segitiga(int alas, int tinggi) {
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+   
+    
+   @Override
+   public float luas(){
+       return this.alas * this.tinggi;
+   }
+}
+class Lingkaran extends BangunDatar {
+    int r;
+
+    public Lingkaran(int r) {
+        this.r = r;
+    }
+    
+    @Override
+    public float luas(){
+        return (float) (Math.PI * r * r);
+    }
+    
+    @Override
+    public float keliling(){
+        return (float) (2 * Math.PI * r);
+    }
+    
+}
+
+public class TugasOOP3 {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+            BangunDatar bangunDatar = new BangunDatar();
+        Persegi persegi = new Persegi(4);
+        Segitiga segitiga = new Segitiga(6, 3);
+        Lingkaran lingkaran = new Lingkaran(50);
+        
+        // memanggil method luas dan keliling
+        bangunDatar.luas();
+        bangunDatar.keliling();
+        
+        System.out.println("Luas persegi: " + persegi.luas());
+        System.out.println("keliling persegi: " + persegi.keliling());
+        System.out.println("Luas segitiga: " + segitiga.luas());
+        System.out.println("Luas lingkaran: " + lingkaran.luas());
+        System.out.println("keliling lingkaran: " + lingkaran.keliling());
+    }
+    
+}
